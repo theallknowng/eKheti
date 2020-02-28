994186f8c0a4e5c052660f0d0c175330ea5d08cd
Alok Pandey <alokrocks217@gmail.com>
2020-02-28 21:08:28 +0530

    Heroku

--- config/database-connection.js
+++ config/database-connection.js
@@ -2,7 +2,6 @@ var mysql = require('mysql')
 const dotenv = require('dotenv');
 dotenv.config();
 
-
 // var connection = mysql.createConnection({
 //   host: process.env.host,
 //   user: process.env.user,
