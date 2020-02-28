4859ec2d5932c4940e554ba424c44db7879d8e58
Alok Pandey <alokrocks217@gmail.com>
2020-02-28 12:06:05 +0530

    Android sor

--- config/database-connection.js
+++ config/database-connection.js
@@ -2,6 +2,7 @@ var mysql = require('mysql')
 const dotenv = require('dotenv');
 dotenv.config();
 
+
 // var connection = mysql.createConnection({
 //   host: process.env.host,
 //   user: process.env.user,
